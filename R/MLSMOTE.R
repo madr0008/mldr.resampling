02124414675e56605c22c92db1cb53ce3940ddbc
Miguel Angel <madr0008@red.ujaen.es>
2023-02-16 11:39:30 +0100

    Created utils file

--- R/MLSMOTE.R
+++ R/MLSMOTE.R
@@ -1,34 +1,3 @@
-#' Auxiliary function used by MLSMOTE. Creates a synthetic sample based on values of attributes and labels of its neighbors
-#'
-#' @param seedInstance Sample we are using as "template"
-#' @param refNeigh Reference neighbor
-#' @param neighbors Neighbors to take into account
-#' @param D mld \code{mldr} object with the multilabel dataset to preprocess
-#'
-#' @return A synthetic sample derived from the one passed as a parameter and its neighbors
-#' @examples
-#' \dontrun{
-#' library(mldr)
-#' newSample(25,28,c(15,28,62),bibtex)
-#' }
-newSample <- function(seedInstance, refNeigh, neighbors, D) {
-
-  c(
-    lapply(D$attributesIndexes, function(i) { #Attributes
-      ifelse(D$attributes[[i]] %in% c("numeric", "Date"),
-             D$dataset[seedInstance,i] + (D$dataset[refNeigh,i] - D$dataset[seedInstance,i])*stats::runif(1, 0, 1), #Numeric attributes
-             utils::tail(names(sort(table(D$dataset[neighbors, i]))), 1)) #Non numeric attributes
-    }),
-    sapply(lapply(D$dataset[c(seedInstance, neighbors),D$labels$index], sum), function(x) { #Labels
-      ifelse(x > ((length(neighbors)+1)/2), 1, 0)
-    }),
-    rep(NA, length(D$dataset) - D$measures$num.attributes) #Other measures like labelcount, SCUMBLE
-  )
-
-}
-
-
-
 #' @title Synthetic oversampling of multilabel instances (MLSMOTE)
 #'
 #' @description This function implements the MLSMOTE algorithm. It is a preprocessing algorithm for imbalanced multilabel datasets,
@@ -51,7 +20,7 @@ MLSMOTE <- function(D, k) {
   newSamples <- unlist(pbapply::pblapply(D$labels[D$labels$IRLbl > D$measures$meanIR,]$index, function(x) {
                                   minBag <- as.numeric(rownames(D$dataset[D$dataset[x]==1,]))
                                   lapply(minBag, function(y) {
-                                    neighbors <- minBag[order(mldr.resampling::calculateDistances(y, minBag, x, D))[1:k+1]]
+                                    neighbors <- minBag[order(calculateDistances(y, minBag, x, D))[1:k+1]]
                                     refNeigh <- sample(neighbors, size=1)
                                     newSample(y, refNeigh, neighbors, D)
                                   })
--- R/utils.R
+++ R/utils.R
@@ -6,7 +6,6 @@
 #' @param D mld \code{mldr} object with the multilabel dataset to preprocess
 #'
 #' @return A list with the distance to the rest of samples
-#' @export
 #'
 #' @examples
 #' \dontrun{
@@ -38,3 +37,36 @@ calculateDistances <- function(sample, rest, label, D) {
   })
 
 }
+
+
+
+#' Auxiliary function used by MLSMOTE. Creates a synthetic sample based on values of attributes and labels of its neighbors
+#'
+#' @param seedInstance Sample we are using as "template"
+#' @param refNeigh Reference neighbor
+#' @param neighbors Neighbors to take into account
+#' @param D mld \code{mldr} object with the multilabel dataset to preprocess
+#'
+#' @return A synthetic sample derived from the one passed as a parameter and its neighbors
+#' @examples
+#' \dontrun{
+#' library(mldr)
+#' newSample(25,28,c(15,28,62),bibtex)
+#' }
+newSample <- function(seedInstance, refNeigh, neighbors, D) {
+
+  c(
+    lapply(D$attributesIndexes, function(i) { #Attributes
+      ifelse(D$attributes[[i]] %in% c("numeric", "Date"),
+             D$dataset[seedInstance,i] + (D$dataset[refNeigh,i] - D$dataset[seedInstance,i])*stats::runif(1, 0, 1), #Numeric attributes
+             utils::tail(names(sort(table(D$dataset[neighbors, i]))), 1)) #Non numeric attributes
+    }),
+    sapply(lapply(D$dataset[c(seedInstance, neighbors),D$labels$index], sum), function(x) { #Labels
+      ifelse(x > ((length(neighbors)+1)/2), 1, 0)
+    }),
+    rep(NA, length(D$dataset) - D$measures$num.attributes) #Other measures like labelcount, SCUMBLE
+  )
+
+}
+
+
