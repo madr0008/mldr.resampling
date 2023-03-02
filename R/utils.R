10137ba75a7dfdb296fd5f0c8f9ccaa4b57f9615
Miguel Angel <madr0008@red.ujaen.es>
2023-03-02 12:54:26 +0100

    Fixed an error

--- R/utils.R
+++ R/utils.R
@@ -22,15 +22,16 @@ calculateDistances <- function(sample, rest, label, D) {
                sum( #For numeric attributes: square of the difference
                  (D$dataset[sample,D$attributesIndexes[D$attributes[1:D$measures$num.inputs]=="numeric"]] - D$dataset[y,D$attributesIndexes[D$attributes[1:D$measures$num.inputs]=="numeric"]])^2
                ),
-               sum( #For non numeric attributes: Value Difference Measure (VDM)
-                 sapply(D$attributesIndexes[D$attributes[1:D$measures$num.inputs]!="numeric"], function(x) {
-                   table1 <- table((D$dataset[D$dataset[x] == D$dataset[sample,x],])[label])/(table(D$dataset[x])[[D$dataset[sample,x]]])
-                   table2 <- table((D$dataset[D$dataset[x] == D$dataset[y,x],])[label])/(table(D$dataset[x])[[D$dataset[y,x]]])
-                   sum(
-                     abs(ifelse(length(table1 == 1), ifelse(names(table1) == "0", stats::setNames(c(table1, 0), c("0","1")), stats::setNames(c(0, table1), c("0","1"))), table1) - ifelse(length(table2 == 1), ifelse(names(table2) == "0", stats::setNames(c(table2, 0), c("0","1")), stats::setNames(c(0, table2), c("0","1"))), table2))
-                   )
-                 })
-               )
+               ifelse(sum(D$attributes[1:D$measures$num.inputs]!="numeric") > 0,
+                 sum( #For non numeric attributes: Value Difference Measure (VDM)
+                   sapply(D$attributesIndexes[D$attributes[1:D$measures$num.inputs]!="numeric"], function(x) {
+                     table1 <- table((D$dataset[D$dataset[x] == D$dataset[sample,x],])[label])/(table(D$dataset[x])[[D$dataset[sample,x]]])
+                     table2 <- table((D$dataset[D$dataset[x] == D$dataset[y,x],])[label])/(table(D$dataset[x])[[D$dataset[y,x]]])
+                     sum(
+                       abs(ifelse(length(table1 == 1), ifelse(names(table1) == "0", stats::setNames(c(table1, 0), c("0","1")), stats::setNames(c(0, table1), c("0","1"))), table1) - ifelse(length(table2 == 1), ifelse(names(table2) == "0", stats::setNames(c(table2, 0), c("0","1")), stats::setNames(c(0, table2), c("0","1"))), table2))
+                     )
+                   })
+                 ), 0)
              )
            )
     )
